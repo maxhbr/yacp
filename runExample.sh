f77c6689afb0f7769446c0cedc29498960cddfaf
Maximilian Huber <gh@maxhbr.de>
2021-04-17 19:29:28 +0200

    add run example script
    Signed-off-by: Maximilian Huber <gh@maxhbr.de>

---
+++ runExample.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+stack build
+stack exec yacp-exe -- \
+  -ort "test/data/analyzer-result.json" \
+  -sc "test/data/bat.scancode.pp.json" \
+  -sc "test/data/black.scancode.pp.json" \
+  -spdx "data/spdx-spdx-spec/examples/SPDXJSONExample-v2.2.spdx.json" \
+  "_tmp"
